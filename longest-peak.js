3b86cb9b5a8e4d561f453595ad28ad6c3a99fd16
Matthew Weisker <mweisker@live.com>
2023-07-18 13:52:40 -0700

    longest-peak optimal solution

--- longest-peak.js
+++ longest-peak.js
@@ -48,3 +48,30 @@ const array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3];
 const array2 = [1, 2, 3, 4, 3, 2, 1, 2]
 const array3 = [1, 3, 2]
 console.log(longestPeak(array))
+
+
+const longestPeakEx = array => {
+  let longestPeakLength = 0;
+  i = 1;
+  while (i < array.length - 1) {
+    const isPeak = array[i - 1] < array[i] && array[i] > array[i + 1];
+    if (isPeak) {
+      let leftIdx = i - 2;
+      while (leftIdx >=0 && array[leftIdx] < array[leftIdx + 1]) {
+        leftIdx--;
+      }
+      let rightIdx = i + 2;
+      while (rightIdx < array.length && array[rightIdx] < array[rightIdx - 1]) {
+        rightIdx++;
+      }
+      currentPeakLength = rightIdx - leftIdx - 1;
+      longestPeakLength = Math.max(longestPeakLength, currentPeakLength);
+      i = rightIdx;
+      } else {
+      i++;
+    }
+  }
+  return longestPeakLength
+}
+
+console.log(longestPeakEx(array))
